fe8f062724961b917f96cf0c535c34c7814704ef
Vishal <vishalkiranpatel@gmail.com>
2025-12-16 11:06:33 +0530

    added update_status() function

--- taskforge/app/main.py
+++ taskforge/app/main.py
@@ -1,29 +1,59 @@
 import sys
-from app.db import init_db, add_task, list_tasks
+from app.db import init_db, add_task, list_tasks, update_status
 
 
 def main():
   init_db()
   
-  if len(sys.argv) < 3:
-    print("Usage: python main.py add \"task description\"")
+  if len(sys.argv) < 2: 
+    print("Usage:")
+    print("   python main.py add \"task description\"")
+    print("   python main.py list")
+    print("   python main.py update task_id")
     return 
   
-  command = sys.argv[1]
+  command = sys.argv[1].lower()
   
   if command == "add":
+    if len(sys.argv) < 3:
+      print("Error: Task description is missing.")
+      print("Usage: python main.py add \"task description\"")
+      return
+    
+    # Join all remaining args in case the description has spaces
     description = " ".join(sys.argv[2:])
     add_task(description)
-    print("Task added succesfully")
+    print(f"Task added: {description}")
     
   elif command == "list":
     tasks = list_tasks()
     if not tasks:
       print("No tasks found")
     else:
-      for task_id, desc in tasks:
-        print(f"{task_id}. {desc}")
+      print("Tasks: ")
+      for task_id, desc, status, created_at in tasks:
+        print(f"{task_id}. Description  : {desc}")
+        print(f"   status       : {status}")
+        print(f"   Created At   : {created_at}")
+        print("-" * 40)
     
+  elif command == "update":
+    if len(sys.argv) < 3:
+      print("Error: Task id is missing.")
+      print("Usage: python main.py update \"task_id\"")
+      return
+    
+    try:
+      task_id = int(sys.argv[2])
+    except ValueError:
+      print("Error: Task id must be a number.")
+      return
+
+    update_status(task_id)
+    print(f"status updated for task ID: {task_id}")
+  else:
+    print(f"Unknown command: {command}")
+    print("Available commands: add, list")
     
 if __name__ == "__main__":
   main()
